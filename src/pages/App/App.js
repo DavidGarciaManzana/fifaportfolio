c2d2b84048897696562180467f5010cf06d75bc8
David Garcia <davidgarciama@outlook.com>
2023-04-22 18:50:25 -0600

    Header (TopBar), Main and Footer added

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import styles from './App.module.css'
 import TopBar from '@/pages/TopBar/TopBar'
+import Main from '@/pages/Main/Main'
+import Footer from "@/pages/Footer/Footer";
 import {LanguageContext} from "@/pages/LanguageProvider/LanguageProvider";
 import Head from "next/head";
 
@@ -11,7 +13,8 @@ function App() {
             <Head><title>{t?.title}</title></Head>
             <div className={styles.appContainer}>
                 <TopBar></TopBar>
-
+                <Main></Main>
+                <Footer></Footer>
             </div>
         </>
     )
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from '@/pages/Main/Main.module.css'
 
 function Main (){
     return (
         <>
+            <main className={styles.main}>
+                <h1>Main</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eleifend est et malesuada ullamcorper. Mauris at mauris iaculis neque vehicula ultrices quis ac massa. Proin vitae erat accumsan, scelerisque mauris sit amet, vulputate tortor. Morbi vel pulvinar ante. Nulla quis commodo risus, semper pharetra nulla. Curabitur venenatis vitae sem eget sagittis. Cras semper nisi ipsum.
+
+                    Integer pretium orci ut rutrum faucibus. Nam tempor porta sapien. Quisque tempus nec ex quis commodo. Fusce vestibulum auctor ipsum, a feugiat dui malesuada et. Quisque vehicula iaculis orci, at dignissim diam dictum vel. Suspendisse aliquet ligula turpis, nec vestibulum ipsum maximus eu. Quisque et mattis sem. Etiam malesuada hendrerit erat sed dignissim. Sed semper facilisis ex, ac vulputate enim auctor sit amet. Donec placerat, nisl ac molestie consectetur, enim orci posuere est, at blandit urna enim ac ligula. Curabitur finibus nunc sed condimentum rutrum. Nulla quis maximus lorem, non luctus metus. Mauris vehicula, sem id placerat mollis, metus dui tempus tortor, eget tincidunt enim velit sollicitudin lacus. Aenean lacinia tellus in sem accumsan accumsan.
+
+                    Duis maximus efficitur ante. Aliquam porttitor diam id tempor vestibulum. Donec consequat ipsum non orci pulvinar, vitae ornare nisl ultrices. Morbi vitae consectetur turpis. Mauris vel purus congue, ultrices tellus eu, pharetra nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum justo eget eleifend interdum. Nullam pharetra quam in eros interdum ornare. Fusce ultricies vel orci et consectetur. Ut nisl velit, iaculis nec rhoncus vitae, fringilla id arcu. Maecenas id mi sit amet felis iaculis ullamcorper. Duis facilisis fermentum dui, non ullamcorper ex maximus nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+                    Mauris at nisi porttitor, tempor ex vitae, posuere sapien. Aliquam viverra eget leo at faucibus. Nullam quis dui in erat dictum sagittis. Mauris sit amet mi vitae justo pretium accumsan. Proin id purus non ex venenatis varius et sit amet orci. Quisque sagittis orci nunc, bibendum pulvinar justo scelerisque nec. Pellentesque porttitor aliquam pharetra. Aliquam et ipsum ut turpis placerat blandit. In vestibulum aliquet quam non consectetur. Cras laoreet, eros ut vehicula gravida, leo nulla lobortis mauris, ac facilisis massa metus sit amet purus. Aenean augue leo, sollicitudin et purus sit amet, porttitor finibus odio. Nulla facilisi. Integer hendrerit lectus dolor, ac placerat justo ornare non. Aenean eu dapibus ipsum, sit amet aliquam metus. Curabitur in porttitor neque, vitae varius dui.
+
+                    Duis tempus rhoncus tortor, nec hendrerit leo sodales non. Quisque ullamcorper velit quis ipsum efficitur, aliquet pulvinar orci volutpat. Aliquam est tortor, ullamcorper sit amet orci ac, venenatis pharetra risus. Phasellus volutpat laoreet ex lacinia iaculis. Nullam placerat nunc quis diam tempor pharetra. Quisque fermentum risus vitae tincidunt scelerisque. Etiam ut eros dui. Proin tincidunt eros nisi, fermentum maximus purus suscipit sed.  </p>
+            </main>
         </>
     )
 }
