2ddb7319f6ab4e83d26be9226aa931ff2dc5ece4
David Garcia <davidgarciama@outlook.com>
2023-04-26 16:07:58 -0600

    Top bar color changed and emojis added to the about me section

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -4,19 +4,19 @@ function AboutMe() {
     return (
         <div className={styles.aboutContainer}>
             <p>
-                David is a Mexican software developer.
+                David is a Mexican 🇲🇽 software developer.
                 He has a passion for learning and is constantly expanding his knowledge base, drawing inspiration
                 from a
                 diverse range of industries to apply to his work.
                 An abstract thinker who is obsessed with details and prides himself on his ability to collaborate
                 with
                 others effectively.
-                He is an excellent communicator, a great listener, and a team player who is always available to help
+                He is an excellent communicator 🎤, a great listener 👂, and a team player who is always available to help
                 his
-                colleagues.
+                colleagues ✌️.
                 With his petroleum engineering background, David brings a unique perspective to his work, and his
                 creativity
-                and innovative thinking make him a valuable asset to any team.
+                and innovative thinking make him a valuable asset to any team 🎨.
             </p>
         </div>
 
