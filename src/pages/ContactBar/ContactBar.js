c8040be3486c5083f6f5b5e86ca763f718824d74
David Garcia <davidgarciama@outlook.com>
2023-04-26 12:30:53 -0600

    Icons in contact bar changed

--- src/pages/ContactBar/ContactBar.js
+++ src/pages/ContactBar/ContactBar.js
@@ -6,19 +6,19 @@ function ContactBar() {
     return (
         <div className={styles.description}>
             <a href="mailto:Davidgarciama@outlook.com" target="_blank">
-                    <Mail color={'white'}/>
+                    <Mail  strokeWidth={1.5} color={'white'}/>
             </a>
             <a href='https://github.com/DavidGarciaManzana' target="_blank">
-                    <GitHub color={'white'}/>
+                    <GitHub strokeWidth={1.5} color={'white'}/>
             </a>
             <a href='https://twitter.com/DavidGarciaMa1' target="_blank">
-                    <Twitter color={'white'}/>
+                    <Twitter strokeWidth={1.5} color={'white'}/>
             </a>
             <a href='https://www.linkedin.com/in/davidgarciaman/' target="_blank">
-                    <Linkedin color={'white'}/>
+                    <Linkedin strokeWidth={1.5}  color={'white'}/>
             </a>
             <a href="https://api.whatsapp.com/send?phone=5547078017" target="_blank">
-                    <MessageSquare color={'white'}/>
+                    <MessageSquare  strokeWidth={1.5} color={'white'}/>
             </a>
 
 
