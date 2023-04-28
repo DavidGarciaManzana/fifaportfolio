b058fe58830545ecf1b2ca4d8d59e667a19ab82f
David Garcia <davidgarciama@outlook.com>
2023-04-28 15:47:49 -0600

    Projects section finished

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -6,11 +6,11 @@ import UniqueProject from "@/pages/UniqueProject/UniqueProject";
 function Projects() {
     return (
         <div className={styles.projectsContainer}>
-            <UniqueProject picture={'lingualentoWeb.png'} link={'https://lingualento.com'} title={'Lingualento'}>
+            <UniqueProject languages={['https://purecatamphetamine.github.io/country-flag-icons/3x2/US.svg','https://purecatamphetamine.github.io/country-flag-icons/3x2/MX.svg']} picture={'lingualentoWeb.png'} link={'https://lingualento.com'} title={'Lingualento'}>
                 Write a message, define a tone, a format and a size and you'll get
                 your message re-factorized
             </UniqueProject>
-            <UniqueProject picture={'refrichefWeb.png'} link={'https://refrichef.vercel.app'} title={'Refrichef'}>
+            <UniqueProject languages={['https://purecatamphetamine.github.io/country-flag-icons/3x2/MX.svg']} picture={'refrichefWeb.png'} link={'https://refrichef.vercel.app'} title={'Refrichef'}>
                 Put the ingredients you have on your fridge and you'll get a set of ideas
                 for dishes you can cook.
             </UniqueProject>
--- src/pages/UniqueProject/UniqueProject.js
+++ src/pages/UniqueProject/UniqueProject.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import styles from '@/pages/UniqueProject/UniqueProject.module.css'
 import {ArrowRight} from 'react-feather'
-function UniqueProject({picture, title,link, children}) {
+
+function UniqueProject({picture, title, link, languages = '', children}) {
+    
     return (
         <div className={styles.projectContainer}>
-            <img src={picture} alt=""/>
+            <img className={styles.webPicture} src={picture} alt=""/>
             <div className={styles.text}>
-                <h1>{title}</h1>
+                <div className={styles.titleFlagContainer}>
+                    <h1>{title}</h1>
+                    {languages.length > 0 && languages.map((lang) => (
+                        <img src={lang} alt=""/>
+                    ))}
+                </div>
+
                 <p>{children}</p>
                 <a href={link} target={"_blank"}>
                     <p>View Project <ArrowRight strokeWidth={2} height={20}/></p>
                 </a>
+
             </div>
+
         </div>
     );
 }
