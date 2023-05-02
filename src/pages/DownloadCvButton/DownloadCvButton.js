03f01846419660bc96e93b312af8c00cedf102c9
David Garcia <davidgarciama@outlook.com>
2023-05-02 14:00:00 -0600

    Download cv button added in both languages (Now download the correct pdf each time

--- src/pages/DownloadCvButton/DownloadCvButton.js
+++ src/pages/DownloadCvButton/DownloadCvButton.js
@@ -2,11 +2,20 @@ import React from 'react'
 import styles from '@/pages/DownloadCvButton/DownloadCvButton.module.css'
 import Button from "@/pages/Button/Button";
 import {LanguageContext} from "@/pages/LanguageProvider/LanguageProvider";
+import {useRouter} from "next/router";
 
 function DownloadCvButton() {
+    const {locale} = useRouter();
     const {t} = React.useContext(LanguageContext);
+    const downloadRef = React.useRef(null);
+
+    const handleDownload = () => {
+        downloadRef.current.click();
+    };
     return (
-        <Button className={styles.button}>{t?.cv}</Button>
+        <Button onClick={handleDownload} className={styles.button}>{t?.cv}
+            {locale==='en' ? <a ref={downloadRef} href="En_Cv_David_Garcia.pdf" download="En_Cv_David_Garcia"/>:<a ref={downloadRef} href="Es_Cv_David_Garcia.pdf" download="Es_Cv_David_Garcia"/>}
+        </Button>
     );
 }
 
